19033b2fc9307f8098eb84cc88e41f07c0181b50
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-06-15 00:27:37 -0400

    Added validates

--- app.js
+++ app.js
@@ -25,7 +25,25 @@ const promptUser = () => {
             type: 'input',
             name: 'about',
             message: 'Provide some information about yourself:'
-        }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+          },
+          {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+              if (confirmAbout) {
+                return true;
+              } else {
+                return false;
+              }
+            }
+          }
     ]);
 };
 const promptProject = portfolioData => {
@@ -42,12 +60,28 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
-        },
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log('Please enter your name!');
+                return false;
+              }
+            }
+          },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                  return true;
+                } else {
+                  console.log('Please enter your name!');
+                  return false;
+                }
+              }
         },
         {
             type: 'checkbox',
@@ -58,7 +92,15 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: urlInput => {
+                if (urlInput) {
+                  return true;
+                } else {
+                  console.log('Please enter your name!');
+                  return false;
+                }
+              }
         },
         {
             type: 'confirm',
@@ -74,7 +116,6 @@ const promptProject = portfolioData => {
         }
     ])
         .then(projectData => {
-            portfolioData.projects.push(projectData).then(projectData)
             portfolioData.projects.push(projectData);
             if (projectData.confirmAddProject) {
                 return promptProject(portfolioData);
