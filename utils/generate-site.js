2f24a5033fd72e158a349333ec68d709377515d6
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-06-26 01:35:10 -0400

    1

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -18,7 +18,7 @@ const writeFile = fileContent => {
     });
   };
 const copyFile = () => {
-    return new Promise((resolve,rejcet) => {
+    return new Promise((resolve,reject) => {
         fs.copyFile('./src/style.css', './dist/style.css', err => {
             if (err) {
               reject(err);
